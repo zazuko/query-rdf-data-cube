e578b085a4edb826ff0f45a7b538d2f48ec1964d
Victor Felder <victor@draft.li>
2019-09-09 15:09:05 +0200

    test(serialization): de/serialization is tested

---
+++ src/__tests__/serialization.test.ts
@@ -0,0 +1,82 @@
+import { literal, namedNode } from "@rdfjs/data-model";
+import Component from "../components";
+import Attribute from "../components/attribute";
+import Dimension from "../components/dimension";
+import Measure from "../components/measure";
+import { DataCube } from "../datacube";
+import DataSet from "../dataset";
+
+const cube = new DataCube("https://ld.stadt-zuerich.ch/query", { languages: ["it", "ru"]});
+
+describe("datacube", () => {
+  beforeAll(async () => {
+    await cube.datasets();
+  });
+
+  it("serializes", () => {
+    const serialized = cube.toJSON();
+    expect(serialized).toMatchSnapshot();
+  });
+  it("serialization is idempotent", () => {
+    const serialized = cube.toJSON();
+    expect(DataCube.fromJSON(serialized).toJSON()).toBe(serialized);
+  });
+  it("deserializes", () => {
+    const serialized = cube.toJSON();
+    expect(DataCube.fromJSON(serialized)).toBeInstanceOf(DataCube);
+  });
+});
+
+describe("dataset", () => {
+  beforeAll(async () => {
+    await cube.datasets();
+  });
+
+  it("serializes", async () => {
+    const dataset = (await cube.datasets())[0];
+    const serialized = dataset.toJSON();
+    expect(serialized).toMatchSnapshot();
+  });
+  it("serialization is idempotent", async () => {
+    const dataset = (await cube.datasets())[0];
+    const serialized = dataset.toJSON();
+    expect(DataSet.fromJSON(serialized).toJSON()).toBe(serialized);
+  });
+  it("deserializes", async () => {
+    const dataset = (await cube.datasets())[0];
+    const serialized = dataset.toJSON();
+    expect(DataSet.fromJSON(serialized)).toBeInstanceOf(DataSet);
+  });
+});
+
+describe("component", () => {
+  beforeAll(async () => {
+    await cube.datasets();
+  });
+
+  it("serializes", async () => {
+    const dataset = (await cube.datasets())[0];
+    const components = (await dataset.dimensions())[0];
+    const serialized = components.toJSON();
+    expect(serialized).toMatchSnapshot();
+  });
+  it("serialization is idempotent", async () => {
+    const dataset = (await cube.datasets())[0];
+    const components = (await dataset.dimensions())[0];
+    const serialized = components.toJSON();
+    expect(Component.fromJSON(serialized).toJSON()).toBe(serialized);
+  });
+  it("deserializes", async () => {
+    const dataset = (await cube.datasets())[0];
+    const dimensions = (await dataset.dimensions())[0];
+    expect(Dimension.fromJSON(dimensions.toJSON())).toBeInstanceOf(Dimension);
+    const measures = (await dataset.measures())[0];
+    expect(Measure.fromJSON(measures.toJSON())).toBeInstanceOf(Measure);
+    const attributes = (await dataset.attributes())[0];
+    expect(Attribute.fromJSON(attributes.toJSON())).toBeInstanceOf(Attribute);
+
+    expect(Measure.fromJSON(dimensions.toJSON())).toBeInstanceOf(Dimension);
+    expect(Attribute.fromJSON(dimensions.toJSON())).toBeInstanceOf(Dimension);
+    expect(Component.fromJSON(dimensions.toJSON())).toBeInstanceOf(Dimension);
+  });
+});
